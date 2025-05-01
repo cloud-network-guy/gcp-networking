db7bee850c1921ed96ca08ee119520db00fc9c01
John Heyer <jheyer@otldap.net>
2025-05-01 09:34:36 -0700

    updates

--- get_shared_vpc_info.py
+++ get_shared_vpc_info.py
@@ -14,7 +14,7 @@ async def main():
 
     projects = await get_projects(access_token)
     project_numbers_to_id = {p.number: p.id for p in projects}
-
+    
     session = ClientSession(raise_for_status=False)
 
     # Get the Shared VPC host project for each service project
@@ -24,8 +24,9 @@ async def main():
 
     # Form new dictionary with Service Project ID as key, host project ID as value
     xvpc_service_projects = {sp: hp for sp, hp in results.items() if hp}
+    # Also create a dictionary of host projects with 
     xvpc_host_projects = {k: [] for k in set(xvpc_service_projects.values())}
-
+    
     # Get all networks defined in the host project
     xvpc_networks = {xvpc_host_project: [] for xvpc_host_project in xvpc_host_projects.keys()}
     for host_project_id in xvpc_host_projects.keys():
@@ -42,13 +43,6 @@ async def main():
         all_subnets.extend(v)
 
 
-
-    #subnet_bindings = {subnet.key: [] for subnet in all_subnets}
-    #for host_project, subnets in subnets.items():
-        #urls = [f"/compute/v1/projects/{host_project}/regions/{s.region}/subnetworks/{s.name}/getIamPolicy" for s in subnets]
-        #tasks = [get_api_data(session, url, access_token) for url in urls]
-        #_ = await gather(*tasks)
-        #results = dict(zip([_.key for _ in all_subnets], _))
     # Get bindings for each Subnet
     tasks = [s.get_bindings(access_token, session) for s in all_subnets]
     _ = await gather(*tasks)
@@ -64,60 +58,34 @@ async def main():
         if v and len(v) == 0:
             print(k, v)
 
-
-    #for subnet in all_subnets:
-    #    print(subnet.key)
-    #    subnet.members = await get_subnet_iam_binding(subnet.id, access_token, session)
-    #    subnet_bindings.update({subnet.key: subnet.members})
-    """
-        for subnet_key, data in results.items():
-            bindings = await get_subnet_iam_binding(subnet.id, access_token, session)
-            if bindings := data.get('bindings'):
-                for binding in bindings:
-                    if binding.get('role') == 'roles/compute.networkUser':
-                        if members := binding.get('members', []):
-                            members = [member for member in members if not member.startswith('deleted')]
-            else:
-                members = []
-            subnet_bindings.update({subnet_key: members})
-        print(subnet_bindings)
-
-        result = results[0]
-        if bindings := result.get('bindings'):
-            for binding in bindings:
-                if binding.get('role') == 'roles/compute.networkUser':
-                    if members := binding.get('members', []):
-                        members = [member for member in members if not member.startswith('deleted')]
-                        xvpc_host_projects[host_project] = members
-        print(_)
-    """
-
+    # Find orphans (service projects that don't have any subnet bindings)
     orphans = []
     service_accounts = {p.id: f"serviceAccount:{p.number}-compute@developer.gserviceaccount.com" for p in projects}
-    #service_accounts = {p.id: f"serviceAccount:{p.number}@container-engine-robot.iam.gserviceaccount.com" for p in projects}
     for project_id, service_account in service_accounts.items():
         if project_id in xvpc_host_projects:
-            #print("Skipping", project_id, "as it's a shared VPC host project")
+            print("Skipping", project_id, "as it's a shared VPC host project")
             continue
         if host_project_id := xvpc_service_projects.get(project_id):
             members = xvpc_host_projects.get(host_project_id, [])
-            #print(service_account, members)
+            #print(service_account)
             if service_account not in members:
                 print("Project ID", project_id, "is missing permissions for", service_account)
                 orphans.append(project_id)
         else:
-            #print("No shared VPC found for project ID", project_id)
+            print("No shared VPC found for project ID", project_id)
             continue
 
     await session.close()
-    #print(len(orphans))
+    print("Found", len(orphans), "orphans:", orphans)
     #quit()
     for orphan in orphans:
         project = [p for p in projects if p.id == orphan]
-        print("orphan:", project)
-
+       # print("orphan:", project)
+    return
+    # Populate Shared VPC Host Projects
     xvpc_host_projects = {}
     for project_id, result in results.items():
+        print(project_id, result)
         if result and len(result) > 0:
             if xvpc_host_project := result[0].get('name'):
                 xvpc_host_projects.update({project_id: xvpc_host_project})
